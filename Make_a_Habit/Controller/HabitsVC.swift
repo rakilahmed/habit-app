81a7b9a28cdbd3ab595ea1bfe0d3f137a4c42883
Md Rakil Ahmed <imrakilahmed@gmail.com>
2020-01-13 05:19:31 -0500

    HabitsVC and Outlets added

---
+++ Make_a_Habit/Controller/HabitsVC.swift
@@ -0,0 +1,24 @@
+//
+//  HabitsVC.swift
+//  Make_a_Habit
+//
+//  Created by Rakil Ahmed on 1/13/20.
+//  Copyright © 2020 Rakil Ahmed. All rights reserved.
+//
+
+import UIKit
+
+class HabitsVC: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func addHabitBtnPressed(_ sender: Any) {
+        print("Button pressed!")
+    }
+}
